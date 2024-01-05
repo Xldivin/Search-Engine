33e138b08a686e0f977398de300ce6305bf822bf
Axel <apple@Apples-MacBook-Pro.local>
2024-01-05 15:38:12 +0200

    added the host that allowed

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -10,7 +10,7 @@ Rails.application.configure do
   # and those relying on copy on write to perform better.
   # Rake tasks automatically ignore this option for performance.
   config.eager_load = true
-  config.hosts << "hj-search.onrender.com"
+  config.hosts << "search-engine-t9zw.onrender.com"
 
   # Full error reports are disabled and caching is turned on.
   config.consider_all_requests_local       = false

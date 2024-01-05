7c891be419c4b31326bc95e04d08f56c4922a943
Axel <apple@Apples-MacBook-Pro.local>
2024-01-05 18:25:07 +0200

    added confing host

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -10,7 +10,7 @@ Rails.application.configure do
   # and those relying on copy on write to perform better.
   # Rake tasks automatically ignore this option for performance.
   config.eager_load = true
-  config.hosts << "search-engine-2oj2.onrender.com"
+  config.hosts << "search-engine-u46o.onrender.com"
 
   # Full error reports are disabled and caching is turned on.
   config.consider_all_requests_local       = false
